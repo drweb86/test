6897ecbdae9317d364d1c9cf3025499c53f0e1ff
Siarhei Kuchuk <Cuchuk.Sergey@gmail.com>
2018-10-13 01:31:01 +0300

    Add SuperPerson

---
+++ ConsoleApplication1/ConsoleApplication1/SuperIgor.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "SuperIgor.h"
+
+
+SuperIgor::SuperIgor()
+{
+}
+
+
+SuperIgor::~SuperIgor()
+{
+}
---
+++ ConsoleApplication1/ConsoleApplication1/SuperIgor.h
@@ -0,0 +1,8 @@
+#pragma once
+class SuperIgor
+{
+public:
+	SuperIgor();
+	~SuperIgor();
+};
+
